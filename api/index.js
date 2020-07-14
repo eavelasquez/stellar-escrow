b52845c2cc81e8cbd53383b05d18f0d2e353baba
Esteban Velasquez <eavc.30@gmail.com>
2020-07-14 18:27:12 -0500

    feat(back): add connection stellar server

--- api/index.js
+++ api/index.js
@@ -1,1 +1,8 @@
 'use strict'
+
+const StellarSdk = require('stellar-sdk')
+
+// instantiate the server.
+const server = new StellarSdk.Server('https://horizon-testnet.stellar.org')
+
+export { server }

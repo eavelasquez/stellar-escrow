65e0bdc2e0843c0324b350a8f8599ac63e756fa5
Esteban Velasquez <eavc.30@gmail.com>
2020-07-14 19:40:44 -0500

    feat(front): add inquirer prompts to the user

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 'use strict'
+
+const inquirer = require('inquirer')
+const { createEscrow } = require('./functions/alice/create-escrow')
+const { fundEscrow } = require('./functions/alice/fund-escrow')
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'who',
+      message: 'Who you are?',
+      choices: ['Alice', 'Bob']
+    }
+  ])
+  .then((answers) => {
+    if (answers.who === 'Alice') {
+      inquirer.prompt([
+        {
+          type: 'list',
+          name: 'action',
+          message: 'What do you want to do?',
+          choices: ['Create Escrow', 'Fund Escrow']
+        }
+      ]).then(async (answers) => {
+        if (answers.action === 'Create Escrow') {
+          createEscrow()
+        }
+        if (answers.action === 'Fund Escrow') {
+          fundEscrow()
+        }
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+  })
+  .catch((error) => {
+    console.log(error)
+  })
